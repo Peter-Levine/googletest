09b82bfa427e2b296156992fe29e5e57b610748c
Peter Levine <plevine457@gmail.com>
2019-06-19 14:21:28 -0400

    Update install-sandbox.sh

--- ci/install-sandbox.sh
+++ ci/install-sandbox.sh
@@ -34,4 +34,4 @@ VER=2.17
 wget http://distfiles.gentoo.org/distfiles/sandbox-${VER}.tar.xz
 tar -xvf sandbox-${VER}.tar.xz
 pushd sandbox-${VER} && ./configure --prefix=/usr --libdir=/usr/lib --sysconfdir=/etc && make && sudo make install && popd
-sandbox ./travis.sh
+sandbox ./ci/travis.sh
